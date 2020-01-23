412a4af1123861dd965585642b5af42c0ef5e6b9
Max Stern <stern.maxwell@gmail.com>
2020-01-22 16:57:18 -0800

    styling

---
+++ main.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('passwordForm');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  //   const password = generatePassword();
+  console.log('submit');
+});
