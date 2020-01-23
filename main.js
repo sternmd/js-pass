0be13ee134caf477a8388ff7f3d73d77ace15fa7
Max Stern <stern.maxwell@gmail.com>
2020-01-23 14:02:42 -0800

    defining constants

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const form = document.getElementById('passwordForm');
 
+const range = document.getElementById('amountRange'); // 8-50
+const uppercase = document.getElementById('uppercase');
+const lowercase = document.getElementById('lowercase');
+const numbers = document.getElementById('numbers');
+const symbols = document.getElementById('symbols');
+
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  //   const password = generatePassword();
-  console.log('submit');
+  const amount = range.value;
+  const includeUppercase = uppercase.checked;
+  const includeLowercase = lowercase.checked;
+  const includeNumbers = numbers.checked;
+  const includeSymbols = symbols.checked;
+  const password = generatePassword(amount, uppercase, symbols, numbers);
+  // console.log('submit');
+  // console.log(amount);
+  // console.log(includeUppercase);
 });
+
+function generatePassword() {}
